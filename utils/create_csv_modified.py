d96da009323f99046277f609671fe1df1f4759ed
Ammar Sufyan <63483228+neouranos@users.noreply.github.com>
2023-06-05 11:47:33 +0700

    add utils

---
+++ utils/create_csv_modified.py
@@ -0,0 +1,36 @@
+# Script to create CSV data file from Pascal VOC annotation files
+# Based off code from GitHub user datitran: https://github.com/datitran/raccoon_dataset/blob/master/xml_to_csv.py
+
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text,
+                     int(root.find('size/width').text),
+                     int(root.find('size/height').text),
+                     member.find('name').text,
+                     int(member.find('bndbox/xmin').text),
+                     int(member.find('bndbox/ymin').text),
+                     int(member.find('bndbox/xmax').text),
+                     int(member.find('bndbox/ymax').text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
+def main():
+    for folder in ['train','validation']:
+        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+        xml_df = xml_to_csv(image_path)
+        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
+        print('Successfully converted xml to csv.')
+
+main()
